8d046ee01c2ce7c4e0ac14cc34fe27051f0a5630
Megan Horsfield <meganthadev@gmail.com>
2020-06-28 03:07:10 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,38 +1,28 @@
 class ArtistsController < ApplicationController
   
-  def new 
-    @artist = Artist.new
-  end 
-  
-  def create 
-    @artist = Artist.new(artist_params)
-    @artist.save
-    redirect_to artist_path(@artist)
-  end   
-  
-  def show 
-    @artist = Artist.find(params[:id])
-  end 
-  
-  def index 
-    @artists = Artist.all
-  end 
-  
-  def edit 
-    @artist = Artist.find(params[:id])
-  end 
-  
-  def update 
-    @artist = Artist.find(params[:id])
-    @artist.update(artist_params)
-    redirect_to artist_path(@artist)
-  end 
-  
-   private
+  def new
+        @artist = Artist.new
+    end
 
-  def artist_params(*args)
-    params.require(:artist).permit(*args)
-  end
+    def edit
+        @artist = Artist.find(params[:id])
+    end
+
+    def create
+        @artist = Artist.new(params.require(:artist).permit(:name, :bio))
+        @artist.save
+        redirect_to @artist
+    end
+
+    def update
+        @artist = Artist.find(params[:id])
+        @artist.update(params.require(:artist).permit(:name, :bio))
+        redirect_to @artist
+    end
+
+    def show
+        @artist = Artist.find(params[:id])
+    end
   
 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,6 @@
 class Song < ApplicationRecord
+  
+  belongs_to :artist 
+  belongs_to :genre 
+  
 end
